3e7d920e71becf1b2d1e310585c8dc1911d8223f
renoir <renoirsousa@gmail.com>
2017-10-12 18:31:44 -0300

    the second exercise

---
+++ leap/leap.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function (input) {
+  this.year = input
+};
+Year.prototype.isLeap = function () {
+  var year = this.year
+  if ( year % 400 === 0 ) {
+    return true
+  }
+  if ( year % 100 !== 0 && year % 4 === 0 ) {
+    return true
+  }else {
+    return false
+  }
+};
+module.exports = Year;
